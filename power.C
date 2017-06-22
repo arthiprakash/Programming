a9b76a9315d3abba321e1d509788350e983e5513
arthiprakash <d.arthi.becs@gmail.com>
2017-06-22 09:13:01 +0530

    Power of a number
    C Program

---
+++ power.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,p;
+printf("Enter the number:\n");
+scanf("%d",&a);
+printf("Enter the exponent");
+scanf("%d",&p);
+b=a^p;
+printf("The output is:/n",b);
+}
