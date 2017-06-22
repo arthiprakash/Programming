c946e0dd3c9d432c43983693796a288aa9fd4223
arthiprakash <d.arthi.becs@gmail.com>
2017-06-22 10:23:53 +0530

    Factorial or not
    C program

---
+++ Factorial.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,factorial=1,n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+for(i=1;i<n;i++)
+{
+factorial=factorial*i;
+}
+printf("the output is :",factorial);
+}
