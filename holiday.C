0a72008f289c895a236c268a40a107ddefe69d20
arthiprakash <d.arthi.becs@gmail.com>
2017-06-28 14:32:59 +0530

    Holiday or not
    C program

---
+++ holiday.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int day;
+printf("Enter the day:");
+Scanf("%d",day);
+switch(day)
+{
+case Monday:
+printf("True");
+break;
+case tuesday:
+printf("True");
+break;
+case Wednesday:
+printf("True");
+break;
+case Thursday:
+printf("True");
+break;
+case Friday:
+printf("True");
+break;
+case Saturday:
+printf("False");
+break;
+case Sunday:
+printf("False");
+break;
+default:
+printf("Invalid Entry");
+}
+getch();
+}
