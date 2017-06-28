e1681744a8f2459874819f020f168a9847cb7ee3
arthiprakash <d.arthi.becs@gmail.com>
2017-06-28 12:08:43 +0530

    To find sum of the given number
    C program to find sum of all numbers less than the given number

---
+++ sumofno.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int i,a=0,n;
+printf("Enter the number:\n");
+scanf("%d",&n);
+for(i=1,i<n;i++)
+{
+a=a+i;
+}
+printf("The required output is:",a);
+getch();
+}
