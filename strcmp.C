5e17f6cdf58785ce02bbbf2eaaed05272f98cb09
arthiprakash <d.arthi.becs@gmail.com>
2017-06-28 12:41:48 +0530

    string equality
    C program

---
+++ strcmp.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char s1,s2;
+printf("Enter the string1:");
+scanf("%s",s1);
+printf("Enter the String2:");
+scanf("%s",s2);
+if(s1==s2)
+printf("the strings are equal");
+else
+printf("They are unequal");
+}
