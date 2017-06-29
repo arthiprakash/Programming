6b0b1a61627d53d07eaaacac690903c6b3e99a70
arthiprakash <d.arthi.becs@gmail.com>
2017-06-29 14:43:08 +0530

    Add ponter
    C program

---
+++ addptr.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+int a,*b,*c;
+printf("Enter the value for b:");
+scanf("%d",&b);
+printf("Enter the value for c:");
+scanf("%d",&c);
+a=*b+*c;
+printf("The result is:",a);
+getch();
+}
