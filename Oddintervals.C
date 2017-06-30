6830b111f5878555c299f7f8400699206f51d270
arthiprakash <d.arthi.becs@gmail.com>
2017-06-30 10:15:36 +0530

    Odd numbers in intervals
    C program

---
+++ Oddintervals.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,i;
+printf("Enter the initial range:");
+scanf("%d",&a);
+printf("Enter the final range:");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+if(i%2!=0)
+printf("%d",i)}
+}
