38f4a4d89e7f6533f0eb9b4c428f0951d5f7dfc3
arthiprakash <d.arthi.becs@gmail.com>
2017-06-30 10:26:24 +0530

    Update Eveninterval.C

--- Eveninterval.C
+++ Eveninterval.C
@@ -7,7 +7,7 @@ printf("Enter the lowest range:");
 scanf("%d",&a);
 printf("Enter the Upper limit:");
 scanf("%d",&b);
-for(i>=a;i<=b;i++)
+for(i>a;i<=b;i++)
 {
 if(i%2==0)
 printf("The result is %d",i);
