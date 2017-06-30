535d5a4bdeec957f1b10de00b02877406ad9a5c1
arthiprakash <d.arthi.becs@gmail.com>
2017-06-30 10:34:18 +0530

    Prime numbers between intervals
    C program

---
+++ Primeintervals.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio>
+int prime(int)
+main()
+{
+int a,b,i;
+printf("Enter the lowest range:");
+scanf("%d",&a);
+printf("Enter the highest range");
+scanf("%d",&b);
+for(i=a;i<=b;i++)
+{
+if(a%i==0)
+{
+printf("%d is a prime number",p);
+else
+printf("%d is not a prime number",p);
+}}
+}
