f6c16e3d554e3b430bbf7216adb629dd6b8e404c
arthiprakash <d.arthi.becs@gmail.com>
2017-06-30 10:24:24 +0530

    Even numbers between intervals
    C program

---
+++ Eveninterval.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a,b,i;
+printf("Enter the lowest range:");
+scanf("%d",&a);
+printf("Enter the Upper limit:");
+scanf("%d",&b);
+for(i>=a;i<=b;i++)
+{
+if(i%2==0)
+printf("The result is %d",i);
+}
+}
