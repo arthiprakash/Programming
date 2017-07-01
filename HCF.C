851b933fd0c978fa673920f56e9bb78db8878a01
arthiprakash <d.arthi.becs@gmail.com>
2017-07-01 09:45:02 +0530

    HCF of two numbers
    C program

---
+++ HCF.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int HCF(int a, int b);
+main()
+{
+int a,b;
+printf("Enter two values:");
+scanf("%d%d",&a,&b);
+printf("HCF of given numbers are %d",HCD(a,b));
+}
+int hcf(int a,int b)
+if(b!=0)
+return hcf(b,a%b);
+else
+return a;
+}
